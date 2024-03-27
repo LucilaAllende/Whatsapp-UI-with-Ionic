b3bbdd5fc0ac54db1524a33735a1a3d6826461b5
LucilaAllende <abigail_lucii@hotmail.com>
2024-03-27 18:40:04 -0300

    Update chat page header and add name parameter

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+  name = ''
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.name = this.route.snapshot.paramMap.get('name') || ''
   }
 
 }
