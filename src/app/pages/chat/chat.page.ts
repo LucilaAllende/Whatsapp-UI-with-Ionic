0491f91081663667e6a6e1ff0ce6642bc8138840
LucilaAllende <abigail_lucii@hotmail.com>
2024-04-15 09:36:56 -0300

    Refactor chat page to include chat box component and send messages functionality

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+export interface IChat {
+  name: string;
+  message: string;
+  time: string;
+  id: number;
+  sender: number;
+}
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
@@ -9,11 +17,22 @@ import { ActivatedRoute } from '@angular/router';
 export class ChatPage implements OnInit {
 
   name = ''
+  message = ''
+  isLoading = false 
+  currentUserId = 1
+  chats: IChat[] = [
+    {name: 'John Doe', message: 'Hello', time: '12:00 PM', id: 1, sender: 1},
+    {name: 'Pablo Doe', message: 'Hi!', time: '12:01 PM', id: 2, sender: 2},
+  ]
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.name = this.route.snapshot.paramMap.get('name') || ''
+  }
+
+  sendMessage(){
+
   }
 
 }
